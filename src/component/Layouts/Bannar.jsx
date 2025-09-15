8aab2edd306a7456b7084c858a7ad0c1202f7db1
Ritaakterlaboni <ritaakterlaboni4@gmail.com>
2025-09-15 23:15:53 +0600

    updates menu

--- src/component/Layouts/Bannar.jsx
+++ src/component/Layouts/Bannar.jsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Bannar = () => {
+
+   var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    autoplay:true,
+    autoplaySpeed:1000,
+  };
   return (
     <>
         <Link to={'/shop'}>
-         <div className="bg-[url(/src/assets/BannerOne.png)] h-[600px] bg-no-repeat bg-cover bg-center"></div>
+         
+
+    <Slider {...settings}>
+      <div className="">
+        <div className="bg-[url(/src/assets/BannerOne.png)] h-[600px] bg-no-repeat bg-cover bg-center"></div>
+      </div>
+      <div className="">
+        <div className="bg-[url(/src/assets/BannerTwo.png)] h-[600px] bg-no-repeat bg-cover bg-center"></div>
+      </div>
+    </Slider>
         </Link>
        
-
     </>
   )
 }
--- src/component/Layouts/Header.jsx
+++ src/component/Layouts/Header.jsx
@@ -7,8 +7,16 @@ import Menu from '../Menu'
 import { FaSearch,FaUser,FaShoppingCart,FaBars    } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
 
 const Header = () => {
+
+const [showmenu, setShowmenu] = useState(false);
+
+const[show, setShow] =useState(false);
+
+
   return (
     <>
     <Container> 
@@ -49,22 +57,45 @@ const Header = () => {
            <Container >
             <Flex className='py-5 justify-between'>
                     
-                        <div className="flex items-center gap-3">
-                            <FaBars />
+                        <div onClick={()=>setShowmenu(!showmenu)} className="flex items-center gap-3 relative">
+                            <FaBars/>
                             <div className="">Shop by Category</div>
+
+
+
+                     {showmenu && (
+                                <div className="bg-black p-[20px] text-white absolute top-[50px] left-[20px]">
+                                    <p className='pb-3'>Accesories</p> 
+                                    <p className='pb-3'>Furniture</p>
+                                    <p className='pb-3'>Clothes</p>
+                                    <p className='pb-3'>Electronics</p>
+                                    <p className=''>Bags</p>
+
+
+
+                                </div>
+                            )}
                         </div>
                         <div className="relative">
                             <input type='text' placeholder='Search Products' className='w-[600px] p-5 bg-white outline-0'/>
                              <FaSearch className='absolute top-1/2 right-4 transform -translate-y-1/2'/>          {/*posotion dile transform -translate-y-1/2 dite hobe */}
                         </div>
                         <div className="flex items-center gap-3">
-                            <div className="flex items-center">
-                                <FaUser />
-                                <IoMdArrowDropdown/>
+                            <div className="flex items-center"  onClick={()=>setShow(!show)}>
+                                <div className=""><FaUser /></div>
+                                <div className="relative"><IoMdArrowDropdown/></div>
+                                 {show &&(
+                                    <div className=" bg-black text-white px-[30px] py-[20px] absolute top-[165px] right-[90px]">
+                                        <p className='pb-4'>My Account</p>
+                                        <p className=''>Log Out</p>
+                                    </div>
+                                )}
                             </div>
                             <FaShoppingCart />
                         </div>
                   
+                               
+
             </Flex>
         </Container>
      </div>
